8ea5d2b1e7f91a12ceb9f1cd04852f7fb35173dc
unknown <Lancopy>
2019-04-04 20:39:32 +0800

    第五次作业，利息计算器

---
+++ 第五次作业，利息计算器/components/custom-tab-bar/index.js
@@ -0,0 +1,30 @@
+Component({
+  properties: {
+    selected: { // 属性名
+      type: Number, 
+      value: 0, 
+      observer(newVal, oldVal, changedPath) {
+      }
+    },
+  },
+  data: {
+    color: "#7A7E83",
+    selectedColor: "#3cc51f",
+   
+  }, 
+  attached() {
+  },
+  onLoad: function (options) {
+
+  },
+  methods: {
+    switchTab(e) {
+      const data = e.currentTarget.dataset
+      const url = data.path
+      wx.switchTab({url})
+      this.setData({
+       /* selected: data.index, */
+      })
+    }
+  }
+})
